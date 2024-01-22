e0703e8c19ebf8f1ca2d70dced871647b48f478c
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-22 17:44:43 +0400

    Update temper.java

--- temper.java
+++ temper.java
@@ -1,15 +1,25 @@
-public class temper{
-	public static void main(String[] arg){
-		int [] x= new int[] {10, 20, 25, 30, 40, 45, 35, 15, 45, 50};
-		int max=x[0];
-		for (int i:x){
-			if (max<i){
-				max=i;
-			}
-			
-		}
-		System.out.println("Maximum:" +max);
-	}
+public class temper {
+  public static void main(String[] arg) {
+    int[] x = new int[] {
+      10,
+      20,
+      25,
+      30,
+      40,
+      45,
+      35,
+      15,
+      45,
+      50
+    };
+    int max = x[0];
+    for (int i: x) {
+      if (max < i) {
+        max = i;
+      }
+
+    }
+    System.out.println("Maximum:" + max);
+  }
 
 }
-		
